e6ab6bf8938d511e414f216a80d157bb2542a907
Partha Sarker <33491861+Glitched519@users.noreply.github.com>
2020-09-09 22:42:23 -0400

    Fixed token vulnerability

--- src/commands/fun/math/calculate.js
+++ src/commands/fun/math/calculate.js
@@ -1,7 +1,10 @@
+const ms = require("ms");
+
 module.exports = {
     run: async(client, message, args) => {
-        if (args.includes("TOKEN")) return;
-        message.channel.send('`'+eval(args)+'`');
+        let msg = args.toLowerCase();
+        if (msg.includes("token")) return;
+        return message.channel.send('`'+eval(args)+'`');
     },  
     aliases: ['eval', 'calc', 'e', 'c'],
     description: 'Evaluates an expression'
