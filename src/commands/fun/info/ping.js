ff73864df9574ebb5306ace131f0249650859848
Partha Sarker <parthawhatever@gmail.com>
2020-11-19 11:02:39 -0500

    improved ping command

--- src/commands/fun/info/ping.js
+++ src/commands/fun/info/ping.js
@@ -4,16 +4,13 @@ module.exports = {
 
         }
         const msg = await message.channel.send({ embed: pingEmbed });
-        msg.edit(
-            pingEmbed = {
-                color: `RANDOM`,
-                title: "Pong!",
-                description: `**Latency:** ${msg.createdTimestamp - message.createdTimestamp}ms\n**API Latency:** ${Math.round(message.client.ws.ping)}ms`,
-                timestamp: new Date()
-            }
-        );
-        msg.delete();
-        await message.channel.send({ embed: pingEmbed });
+        pingEmbed = {
+            color: `RANDOM`,
+            title: "Pong!",
+            description: `**Latency:** ${msg.createdTimestamp - message.createdTimestamp}ms\n**API Latency:** ${Math.round(message.client.ws.ping)}ms`,
+            timestamp: new Date()
+        }
+        msg.edit({embed: pingEmbed});
     },
     aliases: ['alive'],
     description: 'Shows the ping'
