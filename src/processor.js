2a163713e9d02f86fbfbe7f42f061889d532c86d
Partha Sarker <parthawhatever@gmail.com>
2020-11-30 17:02:27 -0500

    test DBL webhooks

--- src/processor.js
+++ src/processor.js
@@ -1,13 +1,13 @@
 const config = require('./config.json');
 const discord = require('discord.js');
 const client = new discord.Client({ partials: ['MESSAGE'] });
-// const DBL = require("dblapi.js");
-// const express = require('express');
-// const http = require('http');
+const DBL = require("dblapi.js");
+const express = require('express');
+const http = require('http');
 
-// const app = express();
-// const server = http.createServer(app);
-// const dbl = new DBL(config["dbl-token"], { webhookPort: 5000, webhookAuth: "ProcessorDaBest", webhookServer: server }, client);
+const app = express();
+const server = http.createServer(app);
+const dbl = new DBL(config["dbl-token"], { webhookPort: 5000, webhookAuth: "ProcessorDaBest", webhookServer: server }, client);
 const { registerCommands, registerEvents } = require('./utils/registry');
 
 (async () => {
@@ -20,25 +20,25 @@ const { registerCommands, registerEvents } = require('./utils/registry');
 	// 	console.log('Server count posted!');
 	// })
 
-	// dbl.on('error', e => {
-	// 	console.log(`Oops! ${e}`);
-	// })
+	dbl.on('error', e => {
+		console.log(`Oops! ${e}`);
+	})
 
-	// dbl.webhook.on('ready', hook => {
-	// 	console.log(`Webhook running at http://localhost:${hook.port}${hook.path}`);
-	// });
+	dbl.webhook.on('ready', hook => {
+		console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+	});
 
-	// dbl.webhook.on('vote', vote => {
-	// 	console.log(`User with ID ${vote.user} just voted!`);
-	// });
+	dbl.webhook.on('vote', vote => {
+		console.log(`User with ID ${vote.user} just voted!`);
+	});
 
-	// app.get('/dblwebhook', (req, res) => {
-	// 	console.log(req);
-	// 	console.log(res);
-	// });
+	app.get('/dblwebhook', (req, res) => {
+		console.log(req);
+		console.log(res);
+	});
 
-	// server.listen(5000, () => {
-	// 	console.log('Listening at http://localhost:5000.');
-	// });
+	server.listen(5000, () => {
+		console.log('Listening at http://localhost:5000.');
+	});
 })();
 
