2a1249cdf299ace908ac0c0a82cfb3e3695e5fff
Partha Sarker <parthawhatever@gmail.com>
2020-12-19 22:06:20 -0500

    updated permissions command

--- src/commands/mod/permissions.js
+++ src/commands/mod/permissions.js
@@ -7,26 +7,37 @@ module.exports = {
             return message.channel.send(":x: **You need the `Manage Messages` permission to view my permissions.**")
         }
         const permissions = [
-            "Administrator",
-            "Send Messages",
-            "Manage Messages",
-            "Manage Roles",
-            "Manage Channels",
-            "Manage Webhooks",
-            "Read Message History",
-            "Change Nickname",
-            "Manage Nicknames",
-            "Kick Members",
-            "Ban Members",
-            "Embed Links",
-            "Attach Files",
-            "Add Reactions",
-            "Connect",
-            "Speak",
-            "Mute Members",
-            "Deafen Members",
-            "Move Members",
-            "Priority Speaker",
+            'Create Instant Invite',
+            'Kick Members',
+            'Ban Members',
+            'Administrator',
+            'Manage Channels',
+            'Manage Guild',
+            'Add Reactions',
+            'View Audit Log',
+            'Priority Speaker',
+            'Stream',
+            'View Channel',
+            'Send Messages',
+            'Send TTS Messages',
+            'Manage Messages',
+            'Embed Links',
+            'Attach Files',
+            'Read Message History',
+            'Mention Everyone',
+            'Use External Emojis',
+            'View Guild Insights',
+            'Connect',
+            'Speak',
+            'Mute Members',
+            'Deafen Members',
+            'Move Members',
+            'Use VAD',
+            'Change Nickname',
+            'Manage Nicknames',
+            'Manage Roles',
+            'Manage Webhooks',
+            'Manage Emojis',
         ]
         permissions.sort();
         const me = message.guild.me;
@@ -41,7 +52,6 @@ module.exports = {
             let permName = perm.toUpperCase().replace(/ /g, "_");
             permEmbed.addField(perm, me.hasPermission(permName) ? yes : no, true);
         });
-        permEmbed.addField(`\u0000`, `\u0000`, true);
 
         if (me.hasPermission('EMBED_LINKS')) message.channel.send(permEmbed);
 
