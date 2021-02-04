67290c6a7d3310ff9eff997dd63d2118920f3417
Glitched519 <parthawhatever@gmail.com>
2021-02-04 18:57:17 -0500

    Post-Verification optimizations

--- src/events/ready.js
+++ src/events/ready.js
@@ -146,8 +146,7 @@ module.exports = class Ready extends BaseEvent {
             const statuses = [
                 `${config.prefix}help`,
                 `using ${os.version()} @${Number.parseFloat(os.cpus()[0].speed / 1000).toPrecision(2)} GHz`,
-                // `${client.users.cache.size} members`,
-                // `${client.guilds.cache.size} servers`,
+                `${client.guilds.cache.size} servers`,
                 `Isopropyl#3066 and Frash#4113`,
             ]
             const status = statuses[Math.floor(Math.random() * statuses.length)];
