febc7cfaa8f3fdaff3734ac2c4ef4bfa59af8eb0
Partha Sarker <parthawhatever@gmail.com>
2021-06-28 15:00:05 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'processor',
     script: './src/processor.js',
+    instances: "max",
+    exec_mode: "cluster",
     watch: false,
     time: true,
   }
-],
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy' : '',
+      'post-deploy': '',
       'pre-setup': ''
     }
   }
