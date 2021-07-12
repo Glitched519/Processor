b2548d92ed91b9ac36d55d68291cfe5c7c2d304f
Partha Sarker <parthawhatever@gmail.com>
2021-07-11 22:35:07 -0400

    Update: Remove unnecessary intents

--- src/processor.js
+++ src/processor.js
@@ -13,13 +13,8 @@ const nonPrivilegedIntents = [
     'GUILD_INTEGRATIONS',
     'GUILD_WEBHOOKS',
     'GUILD_INVITES',
-    'GUILD_VOICE_STATES',
     'GUILD_MESSAGES',
     'GUILD_MESSAGE_REACTIONS',
-    'GUILD_MESSAGE_TYPING',
-    'DIRECT_MESSAGES',
-    'DIRECT_MESSAGE_REACTIONS',
-    'DIRECT_MESSAGE_TYPING'
 ]
 
 const client = new Client({
